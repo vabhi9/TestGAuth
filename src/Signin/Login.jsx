4539736c93f89191bc6ca610313d042633f2184c
Abhinav Verma <abhinavverma2809@gmail.com>
2025-04-28 20:13:32 +0530

    new URL

--- src/Signin/Login.jsx
+++ src/Signin/Login.jsx
@@ -9,7 +9,7 @@ const LoginButton = () => {
       onClick={() =>
         loginWithRedirect({
           authorizationParams: {
-            redirect_uri: "/welcome",
+            redirect_uri: "https://testgauth.onrender.com/welcome",
           },
         })
       }
